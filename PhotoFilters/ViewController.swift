9345f94c6602a7c39547609f44cd8178e67ab4bd
Jeff Chavez <jeffchavez8588@gmail.com>
2014-10-13 09:25:13 -0700

    Initial Commit

---
+++ PhotoFilters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoFilters
+//
+//  Created by Jeff Chavez on 10/13/14.
+//  Copyright (c) 2014 Jeff Chavez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
